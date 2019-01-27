ce1670cbe191af64bb5fbf13ea81ce75d504fc13
chestaylor <cwt5049@rit.edu>
2019-01-27 11:41:21 -0500

    Moverpup collision code more closely resembles the player's

--- moverpup.cpp
+++ moverpup.cpp
@@ -12,7 +12,7 @@ MoverPup::MoverPup(sdlxx::Sdl_renderer& renderer, int x_pos, int y_pos)
     isVillain = false;
     move_speed = rand() % 9 + 2;
     air_move_speed = move_speed - 1;
-    gravity = rand() % 3 + 3;
+    gravity = rand() % 5 + 1;
 
     move_right = rand() % 2;
 }
@@ -21,7 +21,69 @@ void MoverPup::update()
 {
     int jumpchance = rand() % 100;
     if ((velocity.y == 0) && (jumpchance < 1)) {
-        velocity.y -= gravity * 8 - 1; // -1 so the puppers can't doublejump
+        velocity.y -= gravity * 10 - 1; // -1 so the puppers can't doublejump
     }
     Mover::update();
+}
+
+void MoverPup::collide(Game_object& rhs)
+{
+    bool on_top = false;
+    if (rhs.isPlatform && velocity.y > 0) {
+        // We're falling
+        auto overlap_y = (position.y + height + velocity.y) - rhs.position.y;
+        // Check if the bottommost border is already past the objects topmost border
+        bool past_border;
+        if (((position.y + height - velocity.y) - rhs.position.y) > 0) {
+            past_border = true;
+        }
+        else {
+            past_border = false;
+        }
+        if (overlap_y > 0 && past_border == false) {
+            // We're starting to go through a platform.
+
+            // They're no longer jumping
+            //jumping = false;
+            //double_jump = false;
+            velocity.y = 0;
+            position.y = rhs.position.y - height;
+            on_top = true;
+        }
+    }
+
+    // If we're moving left or right and we hit a barrier move back to the last position of the x coordinate
+    if (rhs.isPlatform && move_speed > 0 && on_top == false) {
+        // We're moving to the right
+        // Overlap into the object
+        auto overlap_x = (position.x + width + move_speed) - rhs.position.x;
+        // Check if the leftmost border is already past the objects leftmost border
+        bool past_border;
+        if ((position.x - rhs.position.x) > 0) {
+            past_border = true;
+        }
+        else {
+            past_border = false;
+        }
+        if (overlap_x > 0 && past_border == false) {
+            position.x = rhs.position.x - width;
+            move_right = !move_right;
+        }
+    }
+    else if (rhs.isPlatform && move_speed < 0 && on_top == false) {
+        // We're moving to the left
+        auto overlap_x = (position.x + move_speed) - (rhs.position.x + rhs.width);
+        // Check if the rightmost border is already past the objects leftmost border
+        bool past_border;
+        if (((position.x + width) - (rhs.position.x + rhs.width)) < 0) {
+            past_border = true;
+        }
+        else {
+            past_border = false;
+        }
+        if (overlap_x < 0 && past_border == false) {
+            position.x = rhs.position.x + rhs.width;
+            move_right = !move_right;
+        }
+    }
 }
--- moverpup.h
+++ moverpup.h
@@ -3,11 +3,13 @@
 
 #include "mover.h"
 #include "vec2.h"
+#include "game_object.h"
 
 class MoverPup : public Mover {
 public:
     MoverPup(sdlxx::Sdl_renderer& renderer, int x_pos, int ypos);
     void update() override;
+    void collide(Game_object& rhs) override;
 };
 
 #endif
