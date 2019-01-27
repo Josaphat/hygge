3ce6baebb57eac09a49ea47f1cb7d0b9e52c22a8
chestaylor <cwt5049@rit.edu>
2019-01-26 21:46:18 -0500

    Add Mover superclass

--- minion.cpp
+++ minion.cpp
@@ -3,60 +3,14 @@
 #include "screen_config.h"
 
 Minion::Minion(sdlxx::Sdl_renderer& renderer, int x_pos, int y_pos)
-    : Game_object{50,
-                  50,
-                  {static_cast<double>(x_pos), static_cast<double>(y_pos)},
-                  {0, 0},
-                  true},
-      texture{"resources/minion.bmp", renderer}
-{
-    isPlayer = false;
-    isVillain = true;
-}
-Minion::Minion(sdlxx::Sdl_renderer& renderer)
-    : Game_object{50,
-                  50,
-                  {500, static_cast<double>(window_height - 75)},
-                  {0, 0},
-                  true},
-      texture{"resources/minion.bmp", renderer}
+    : Mover{renderer,
+            50,
+            50,
+            x_pos,
+            y_pos,
+            "resources/minion.bmp"}
 {
     isPlayer = false;
     isVillain = true;
 }
 
-void Minion::update()
-{
-    if (move_right) { position.x += move_speed; }
-    else {
-        position.x -= move_speed;
-    }
-
-    // if we're going to run off the scene switch directions
-    if (position.x >= window_width - 45 || position.x <= 25) {
-        move_right = !move_right;
-    }
-
-    velocity.y += gravity;
-    position.y += velocity.y;
-}
-
-void Minion::draw(sdlxx::Sdl_renderer& renderer)
-{
-    renderer.draw_rect(position.x, position.y, width, height, 0, 0, 255);
-    renderer.copy(texture, int(position.x), int(position.y));
-}
-
-void Minion::collide(Game_object& rhs)
-{
-    if (rhs.can_be_destroyed == false && velocity.y > 0) {
-        // We're falling
-        auto overlap_y = (position.y + height) - rhs.position.y;
-        if (overlap_y > 0) {
-            // We're starting to go through a platform.
-            // Move the player back up so they're not through the platform.
-            position.y -= overlap_y;
-            velocity.y = 0;
-        }
-    }
-}
---
+++ mover.cpp
@@ -0,0 +1,51 @@
+#include "mover.h"
+#include "input_map.h"
+#include "screen_config.h"
+
+Mover::Mover(sdlxx::Sdl_renderer& renderer, int height, int width, int x_pos, int y_pos, std::string path)
+    : Game_object{height,
+                  width,
+                  {static_cast<double>(x_pos), static_cast<double>(y_pos)},
+                  {0, 0},
+                  true},
+    texture{ "resources/minion.bmp", renderer }
+{
+    isPlayer = false;
+    isVillain = true;
+}
+
+void Mover::update()
+{
+    if (move_right) { position.x += move_speed; }
+    else {
+        position.x -= move_speed;
+    }
+
+    // if we're going to run off the scene switch directions
+    if (position.x >= window_width - 45 || position.x <= 25) {
+        move_right = !move_right;
+    }
+
+    velocity.y += gravity;
+    position.y += velocity.y;
+}
+
+void Mover::draw(sdlxx::Sdl_renderer& renderer)
+{
+    renderer.draw_rect(position.x, position.y, width, height, 0, 0, 255);
+    renderer.copy(texture, int(position.x), int(position.y));
+}
+
+void Mover::collide(Game_object& rhs)
+{
+    if (rhs.can_be_destroyed == false && velocity.y > 0) {
+        // We're falling
+        auto overlap_y = (position.y + height) - rhs.position.y;
+        if (overlap_y > 0) {
+            // We're starting to go through a platform.
+            // Move the player back up so they're not through the platform.
+            position.y -= overlap_y;
+            velocity.y = 0;
+        }
+    }
+}
